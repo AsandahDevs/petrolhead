e8d087413602b480ff6df97faa846780d8318295
Asanda <noelmajola@gmail.com>
2024-02-24 23:49:36 +0200

    feat: change from functional to class views

--- car_inventory/german_collection/urls.py
+++ car_inventory/german_collection/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import  path
-from german_collection import views
+from german_collection.views import add_car_manufacturer, manufacturers, update_car_manufacturer_details
 
 app_name = 'german_collection'
 urlpatterns = [
  path('admin/', admin.site.urls),
- path('manufacturers',views.manufacturers,name="manufacturers"),
- path('add-manufacturer', views.add_car_manufacturer, name="add-manufacturer"),
- path('manufacturer/<int:id>', views.update_car_manufacturer_details, name="update-manufacturer")
+ path('manufacturers',manufacturers.as_view(),name="manufacturers"),
+ path('add-manufacturer', add_car_manufacturer.as_view(), name="add-manufacturer"),
+ path('manufacturer/<int:id>', update_car_manufacturer_details.as_view(), name="update-manufacturer")
 ]
--- car_inventory/german_collection/views.py
+++ car_inventory/german_collection/views.py
@@ -1,36 +1,35 @@
 from django.http import Http404, HttpRequest
 from rest_framework import response
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from german_collection.models import manufacturer,car_model,segment
 from .serializer import german_serializer
 
-@api_view(['GET'])
-def manufacturers(request:HttpRequest):
-    #gets list of manufacturers
-    try:
-        car_manufacturers = manufacturer.objects.all()
-    except manufacturer.DoesNotExist:
-        raise Http404('No manufacturer(s) were found')
-    serializer = german_serializer(car_manufacturers,many=True)
-    return response.Response(serializer.data,status=status.HTTP_200_OK)
+class manufacturers(APIView):
+    def get(self,request:HttpRequest):
+        #gets list of manufacturers
+        try:
+            car_manufacturers = manufacturer.objects.all()
+        except manufacturer.DoesNotExist:
+            raise Http404('No manufacturer(s) were found')
+        serializer = german_serializer(car_manufacturers,many=True)
+        return response.Response(serializer.data,status=status.HTTP_200_OK)
 
-@api_view(['POST'])
-def add_car_manufacturer(request:HttpRequest):
-    # adds a new car manufacturer to the database
-    new_car_manufacturer=german_serializer(data=request.data)
-    if new_car_manufacturer.is_valid():
-        new_car_manufacturer.save()
-        return response.Response(new_car_manufacturer.data,status=status.HTTP_201_CREATED)
-    return response.Response(new_car_manufacturer.errors,status=status.HTTP_400_BAD_REQUEST)
+class add_car_manufacturer(APIView):
+    def post(self,request:HttpRequest):
+        # adds a new car manufacturer to the database
+        new_car_manufacturer=german_serializer(data=request.data)
+        if new_car_manufacturer.is_valid():
+            new_car_manufacturer.save()
+            return response.Response(new_car_manufacturer.data,status=status.HTTP_201_CREATED)
+        return response.Response(new_car_manufacturer.errors,status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['PUT'])
-def update_car_manufacturer_details(request:HttpRequest,id):
-    # updates existing car manufacturer details
-    car_manufacturer = manufacturer.objects.get(pk=id)
-    if request.method == 'PUT':
+class update_car_manufacturer_details(APIView):
+    def put(self,request:HttpRequest,id):
+        # updates existing car manufacturer details
+        car_manufacturer = manufacturer.objects.get(pk=id)
         serializer = german_serializer(car_manufacturer,data=request.data)
         if serializer.is_valid():
             serializer.save()
             return response.Response(serializer.data, status=status.HTTP_200_OK)
-    return response.Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        return response.Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
