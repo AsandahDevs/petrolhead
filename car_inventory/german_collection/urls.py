88ac2b7142f54e22a3e64ed4408b7fdef5bc9c09
Asanda <noelmajola@gmail.com>
2024-03-09 23:01:55 +0200

    feat : add endpoint for models

--- car_inventory/german_collection/urls.py
+++ car_inventory/german_collection/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import  path
-from german_collection.views import add_car_manufacturer, delete_manfacturer, manufacturers, update_car_manufacturer_details
+from german_collection.views import add_car_manufacturer, delete_manfacturer, manufacturer_models, manufacturers, update_car_manufacturer_details
 
 app_name = 'german_collection'
 urlpatterns = [
@@ -8,5 +8,6 @@ urlpatterns = [
  path('manufacturers',manufacturers.as_view(),name="manufacturers"),
  path('add-manufacturer', add_car_manufacturer.as_view(), name="add-manufacturer"),
  path('manufacturer/<int:id>', update_car_manufacturer_details.as_view(), name="update-manufacturer"),
- path('delete-manufacturer/<int:id>', delete_manfacturer.as_view(), name="delete-manufacturer")
+ path('delete-manufacturer/<int:id>', delete_manfacturer.as_view(), name="delete-manufacturer"),
+ path('manufacturer-models', manufacturer_models.as_view(),name="manufacturer-models")
 ]
