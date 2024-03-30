05ef801480d7b6cc9fbc8df0a3aaaf1236c35030
Asanda <noelmajola@gmail.com>
2024-03-30 13:39:38 +0200

    update : remove debugger on prod

--- car_inventory/car_inventory/settings.py
+++ car_inventory/car_inventory/settings.py
@@ -18,7 +18,8 @@ SECRET_KEY = env("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env("DEBUG")
+if env("PRODUCTION") == False:
+ DEBUG = env("DEBUG")
 
 ALLOWED_HOSTS = env("ALLOWED_HOSTS").split(",")
 
